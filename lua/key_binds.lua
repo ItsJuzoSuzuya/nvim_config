de260c653257b5f588f4cddac0bf6b20d3398e23
Nick Schefner <schefner20@gmail.com>
2025-04-14 08:36:35 +0200

    Fixed system clipboard bindings and added lsp

--- lua/key_binds.lua
+++ lua/key_binds.lua
@@ -18,64 +18,64 @@ set('o', '<Right>', '<Nop<')
 
 
 -- Telescope #
-local builtin = require("telescope.builtin")
+local builtin = require('telescope.builtin')
 set('n', '<leader>ff', builtin.find_files, {})
 set('n', '<leader>fg', builtin.live_grep, {})
 
 -- Move text up/down #
-set("v", "J", ":m '>+1<CR>gv=gv")
-set("v", "K", ":m '<-2<CR>gv=gv")
+set('v', 'J', ':m ">+1<CR>gv=gv')
+set('v', 'K', ':m "<-2<CR>gv=gv')
 
 -- Keep cursor in the middle #
-set("n", "<C-d>", "<C-d>zz")
-set("n", "<C-u>", "<C-u>zz")
+set('n', '<C-d>', '<C-d>zz')
+set('n', '<C-u>', '<C-u>zz')
 
 -- copy to system clipboard #
-set("n", "y", "\"+y")
-set("v", "y", "\"+y")
+set('n', 'y', '"+y')
+set('v', 'y', '"+y')
 
 -- paste from system clipboard #
-set("n", "p", "\"+p")
-set("v", "p", "\"+p")
+set('n', 'p', '"+p')
+set('v', 'p', '"+p')
 
 -- cut to system clipboard #
-set("n", "c", "\"+c")
-set("v", "c", "\"+c")
+set('n', 'c', '"+c')
+set('v', 'c', '"+c')
 
 -- delete into the void #
-set("n", "d", "\"_d")
-set("v", "d", "\"_d")
+set('n', 'd', '"_d')
+set('v', 'd', '"_d')
 
 -- Dont press Q #
-set("n", "Q", "")
+set('n', 'Q', '')
 
 -- replace all words under cursor #
-set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 --open nvim tree #
-local api = require "nvim-tree.api"
-set("n", "<leader>E", api.tree.open, {})
+local api = require 'nvim-tree.api'
+set('n', '<leader>E', api.tree.open, {})
 
 -- toggle nvim-tree #
-set("n", "<leader>n", api.tree.toggle, {})
+set('n', '<leader>n', api.tree.toggle, {})
 
 -- comment out lines #
-set("v", "<leader>#", [[:s/^/]])
+set('v', '<leader>#', [[:s/^/]])
 
 -- code actions #
-set("n", "<leader>a", vim.lsp.buf.code_action, {})
+set('n', '<leader>a', vim.lsp.buf.code_action, {})
 
 -- Trouble #
-local trouble = require("trouble")
-set("n", "<leader>tt", function() trouble.toggle("document_diagnostics") end)
+local trouble = require('trouble')
+set('n', '<leader>tt', function() trouble.toggle('document_diagnostics') end)
 
 -- Terminal #
-set("n", "<leader>T", [[:terminal<CR>i]])
+set('n', '<leader>T', [[:terminal<CR>i]])
 
 -- Debugging #
-local dap = require("dap")
-set("n", "<leader>db", dap.toggle_breakpoint, {})
-set("n", "<leader>dc", dap.continue, {})
-set("n", "<leader>dn", dap.step_over, {})
-set("n", "<leader>di", dap.step_into, {})
-set("n", "<leader>do", dap.step_out, {})
+local dap = require('dap')
+set('n', '<leader>db', dap.toggle_breakpoint, {})
+set('n', '<leader>dc', dap.continue, {})
+set('n', '<leader>dn', dap.step_over, {})
+set('n', '<leader>di', dap.step_into, {})
+set('n', '<leader>do', dap.step_out, {})
