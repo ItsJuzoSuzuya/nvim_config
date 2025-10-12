3b61acbfb462690ef9c8799a7daf91366d0fec82
ItsJuzoSuzuy <schefner20@gmail.com>
2025-10-13 00:59:37 +0200

    fix everythin (almost)

--- init.lua
+++ init.lua
@@ -1,21 +1,51 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",     -- latest stable release
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
-vim.opt.rtp:prepend(lazypath)
 
+vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.python3_host_prog = '/home/nick/.local/share/pipx/venvs/pip/bin/python'
 
 require "settings"
 require("lazy").setup("plugins")
 require "key_binds"
 
+local dap = require('dap')
+dap.adapters.cpp = {
+  type = 'executable',
+  command = 'gdb',
+  name = 'gdb',
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch with gdb",
+    type = "cpp",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+    args = {},
+    setupCommands = {
+      {
+        text = '-enable-pretty-printing',
+        description = 'enable pretty printing',
+        ignoreFailures = false
+      }
+    }
+  },
+}
