517d81010c1ef31b4cef20dc81ea44876ab95610
ItsJuzoSuzuya <schefner20@gmail.com>
2026-01-04 01:12:59 +0100

    §

--- lua/plugins/dapadapter.lua
+++ lua/plugins/dapadapter.lua
@@ -6,34 +6,33 @@ return {
     "mfussenegger/nvim-dap",
   },
   opts = {
-    ensure_installed = { "cpptools" }, -- mason name for VSCode C++ adapter
+    ensure_installed = {}, -- no unsupported cpptools entry
     handlers = {
       function(config)
-        -- Default handlers
         require("mason-nvim-dap").default_setup(config)
-      end,
-      -- The handler key must match the Mason DAP source name
-      cpptools = function()
-        local dap = require("dap")
-        -- Using GDB's native DAP
-        dap.adapters.cpp = {
-          type = "executable",
-          command = "gdb",
-          args = { "--interpreter=dap" },
-        }
-        dap.configurations.cpp = {
-          {
-            name = "Launch file",
-            type = "cpp",
-            request = "launch",
-            program = function()
-              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-            end,
-            cwd = "${workspaceFolder}",
-            stopOnEntry = false,
-          },
-        }
       end,
     },
   },
+  config = function(_, opts)
+    local dap = require("dap")
+
+    -- GDB native DAP
+    dap.adapters.cpp = {
+      type = "executable",
+      command = "gdb",
+      args = { "--interpreter=dap" },
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "cpp",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+      },
+    }
+  end,
 }
