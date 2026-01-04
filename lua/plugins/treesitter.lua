6c149015b9a8c8d669f937f5f4d0b3dab405abee
ItsJuzoSuzuya <schefner20@gmail.com>
2026-01-04 01:05:51 +0100

    fuck

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,26 +3,27 @@ return {
     "nvim-treesitter/nvim-treesitter",
     enabled = true,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
+    -- load at startup and prefer it earlier in runtimepath
+    lazy = false,
+    priority = 1000,
     dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      {
-        "nvim-treesitter/nvim-treesitter-context",
-        opts = {
-          mode = "cursor",
-          max_lines = 3,
-        },
-      },
+      -- disabled/lazy so they don't get sourced before treesitter
+      -- remove the following entries if you don't want them at all
+      { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false, lazy = true },
+      { "nvim-treesitter/nvim-treesitter-context", enabled = false, lazy = true },
     },
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua",
-          "c",
-          "rust",
-          "markdown",
-          "java",
-        },
+      -- make sure the plugin runtime is added so require() can find lua modules
+      pcall(vim.cmd, "packadd nvim-treesitter")
+
+      local ok, configs_or_err = pcall(require, "nvim-treesitter.configs")
+      if not ok then
+        vim.notify("nvim-treesitter not available; skipping setup: " .. tostring(configs_or_err), vim.log.levels.WARN)
+        return
+      end
+
+      configs_or_err.setup({
+        ensure_installed = { "lua", "c", "rust", "markdown", "java" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
