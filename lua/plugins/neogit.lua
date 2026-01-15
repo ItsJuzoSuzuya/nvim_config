49c48048c51730bca9030c75cc9a0313ce050800
Nick Schefner <schefner20@gmail.com>
2026-01-15 15:29:57 +0100

    neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,9 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",        -- optional
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  cmd = "Neogit",
+}
